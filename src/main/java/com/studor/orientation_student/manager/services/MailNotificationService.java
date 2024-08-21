8334c055de4f7d292b45946487c6d6adea72515c
gedeontiga <gedeontigadev@gmail.com>
2024-08-21 19:00:59 +0100

    Update application.properties for mail sender

--- src/main/java/com/studor/orientation_student/manager/services/MailNotificationService.java
+++ src/main/java/com/studor/orientation_student/manager/services/MailNotificationService.java
@@ -1,18 +1,18 @@
 package com.studor.orientation_student.manager.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.studor.orientation_student.entities.Validation;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
+// @AllArgsConstructor
 @Service
 public class MailNotificationService {
     
-    JavaMailSender javaMailSender;
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     public void sendMail(Validation validation){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
